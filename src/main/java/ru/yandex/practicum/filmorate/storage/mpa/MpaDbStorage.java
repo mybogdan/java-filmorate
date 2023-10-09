90531af10e2e5800d2e5c0b439ee7ea69c6d359e
mybogdan <121495293+mybogdan@users.noreply.github.com>
2023-10-09 08:38:30 +0300

    Update MpaDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDbStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.mpa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
